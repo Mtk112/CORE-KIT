d3fa2951038c006d20c89cab50c3d3f273a6c86a
Mtk112 <impanaza@hotmail.com>
2019-04-19 06:58:18 +0300

    CORE-KITv2

---
+++ inst/www/data/load_profiles.js
@@ -0,0 +1,543 @@
+var load_profiles = [
+  {
+    "hour": 1,
+    "households": 0.0053,
+    "public_utilities": 0.0624,
+    "productive_uses": 0.0342
+  },
+  {
+    "hour": 2,
+    "households": 0.0053,
+    "public_utilities": 0.0624,
+    "productive_uses": 0.0342
+  },
+  {
+    "hour": 3,
+    "households": 0.0053,
+    "public_utilities": 0.0624,
+    "productive_uses": 0.0342
+  },
+  {
+    "hour": 4,
+    "households": 0.0053,
+    "public_utilities": 0.0624,
+    "productive_uses": 0.0342
+  },
+  {
+    "hour": 5,
+    "households": 0.1337,
+    "public_utilities": 0.0624,
+    "productive_uses": 0.0342
+  },
+  {
+    "hour": 6,
+    "households": 0.1337,
+    "public_utilities": 0.0624,
+    "productive_uses": 0.0342
+  },
+  {
+    "hour": 7,
+    "households": 0.0053,
+    "public_utilities": 0.0071,
+    "productive_uses": 0.0342
+  },
+  {
+    "hour": 8,
+    "households": 0.0053,
+    "public_utilities": 0.0071,
+    "productive_uses": 0.0342
+  },
+  {
+    "hour": 9,
+    "households": 0.0053,
+    "public_utilities": 0.0071,
+    "productive_uses": 0.0342
+  },
+  {
+    "hour": 10,
+    "households": 0.0053,
+    "public_utilities": 0.0308,
+    "productive_uses": 0.0505
+  },
+  {
+    "hour": 11,
+    "households": 0.0053,
+    "public_utilities": 0.0308,
+    "productive_uses": 0.0505
+  },
+  {
+    "hour": 12,
+    "households": 0.0053,
+    "public_utilities": 0.0308,
+    "productive_uses": 0.0505
+  },
+  {
+    "hour": 13,
+    "households": 0.0053,
+    "public_utilities": 0.0308,
+    "productive_uses": 0.0505
+  },
+  {
+    "hour": 14,
+    "households": 0.0053,
+    "public_utilities": 0.0308,
+    "productive_uses": 0.0505
+  },
+  {
+    "hour": 15,
+    "households": 0.0053,
+    "public_utilities": 0.0308,
+    "productive_uses": 0.0505
+  },
+  {
+    "hour": 16,
+    "households": 0.0053,
+    "public_utilities": 0.0308,
+    "productive_uses": 0.0505
+  },
+  {
+    "hour": 17,
+    "households": 0.0053,
+    "public_utilities": 0.0071,
+    "productive_uses": 0.0505
+  },
+  {
+    "hour": 18,
+    "households": 0.0245,
+    "public_utilities": 0.0071,
+    "productive_uses": 0.0505
+  },
+  {
+    "hour": 19,
+    "households": 0.198,
+    "public_utilities": 0.0624,
+    "productive_uses": 0.0505
+  },
+  {
+    "hour": 20,
+    "households": 0.1787,
+    "public_utilities": 0.0624,
+    "productive_uses": 0.0505
+  },
+  {
+    "hour": 21,
+    "households": 0.1787,
+    "public_utilities": 0.0624,
+    "productive_uses": 0.0342
+  },
+  {
+    "hour": 22,
+    "households": 0.0631,
+    "public_utilities": 0.0624,
+    "productive_uses": 0.0342
+  },
+  {
+    "hour": 23,
+    "households": 0.0053,
+    "public_utilities": 0.0624,
+    "productive_uses": 0.0342
+  },
+  {
+    "hour": 24,
+    "households": 0.0053,
+    "public_utilities": 0.0624,
+    "productive_uses": 0.0342
+  }
+]
+//Made up office hours load profile.
+var office = [
+  {
+    "hour": 1,
+    "usage": 0.005,
+  },
+  {
+    "hour": 2,
+    "usage": 0.005,
+  },
+  {
+    "hour": 3,
+    "usage": 0.005,
+  },
+  {
+    "hour": 4,
+    "usage": 0.005,
+  },
+  {
+    "hour": 5,
+    "usage": 0.005,
+  },
+  {
+    "hour": 6,
+    "usage": 0.005,
+  },
+  {
+    "hour": 7,
+    "usage": 0.0425,
+  },
+  {
+    "hour": 8,
+    "usage": 0.06455,
+  },
+  {
+    "hour": 9,
+    "usage": 0.103,
+  },
+  {
+    "hour": 10,
+    "usage": 0.103,
+  },
+  {
+    "hour": 11,
+    "usage": 0.103,
+  },
+  {
+    "hour": 12,
+    "usage": 0.103,
+  },
+  {
+    "hour": 13,
+    "usage": 0.103,
+  },
+  {
+    "hour": 14,
+    "usage": 0.103,
+  },
+  {
+    "hour": 15,
+    "usage": 0.103,
+  },
+  {
+    "hour": 16,
+    "usage": 0.06445,
+  },
+  {
+    "hour": 17,
+    "usage": 0.0425,
+  },
+  {
+    "hour": 18,
+    "usage": 0.005,
+  },
+  {
+    "hour": 19,
+    "usage": 0.005,
+  },
+  {
+    "hour": 20,
+    "usage": 0.005,
+  },
+  {
+    "hour": 21,
+    "usage": 0.005,
+  },
+  {
+    "hour": 22,
+    "usage": 0.005,
+  },
+  {
+    "hour": 23,
+    "usage": 0.005,
+  },
+  {
+    "hour": 24,
+    "usage": 0.005,
+  }
+]
+//Made up light hours load profile.
+var light = [
+  {
+    "hour": 1,
+    "usage": 0.005,
+  },
+  {
+    "hour": 2,
+    "usage": 0.005,
+  },
+  {
+    "hour": 3,
+    "usage": 0.005,
+  },
+  {
+    "hour": 4,
+    "usage": 0.005,
+  },
+  {
+    "hour": 5,
+    "usage": 0.005,
+  },
+  {
+    "hour": 6,
+    "usage": 0.0783,
+  },
+  {
+    "hour": 7,
+    "usage": 0.0783,
+  },
+  {
+    "hour": 8,
+    "usage": 0.0783,
+  },
+  {
+    "hour": 9,
+    "usage": 0.0783,
+  },
+  {
+    "hour": 10,
+    "usage": 0.0783
+  },
+  {
+    "hour": 11,
+    "usage": 0.0783,
+  },
+  {
+    "hour": 12,
+    "usage": 0.0787,
+  },
+  {
+    "hour": 13,
+    "usage": 0.0783,
+  },
+  {
+    "hour": 14,
+    "usage": 0.0783,
+  },
+  {
+    "hour": 15,
+    "usage": 0.0783,
+  },
+  {
+    "hour": 16,
+    "usage": 0.0783,
+  },
+  {
+    "hour": 17,
+    "usage": 0.0783,
+  },
+  {
+    "hour": 18,
+    "usage": 0.005,
+  },
+  {
+    "hour": 19,
+    "usage": 0.005,
+  },
+  {
+    "hour": 20,
+    "usage": 0.005,
+  },
+  {
+    "hour": 21,
+    "usage": 0.005,
+  },
+  {
+    "hour": 22,
+    "usage": 0.005,
+  },
+  {
+    "hour": 23,
+    "usage": 0.005,
+  },
+  {
+    "hour": 24,
+    "usage": 0.005,
+  }
+]
+//Made up dark hours load profile.
+var dark = [
+  {
+    "hour": 1,
+    "usage": 0.0787,
+  },
+  {
+    "hour": 2,
+    "usage": 0.0783,
+  },
+  {
+    "hour": 3,
+    "usage": 0.0783,
+  },
+  {
+    "hour": 4,
+    "usage": 0.0783,
+  },
+  {
+    "hour": 5,
+    "usage": 0.0783,
+  },
+  {
+    "hour": 6,
+    "usage": 0.005,
+  },
+  {
+    "hour": 7,
+    "usage": 0.005,
+  },
+  {
+    "hour": 8,
+    "usage": 0.005,
+  },
+  {
+    "hour": 9,
+    "usage": 0.005,
+  },
+  {
+    "hour": 10,
+    "usage": 0.005
+  },
+  {
+    "hour": 11,
+    "usage": 0.005,
+  },
+  {
+    "hour": 12,
+    "usage": 0.005,
+  },
+  {
+    "hour": 13,
+    "usage": 0.005,
+  },
+  {
+    "hour": 14,
+    "usage": 0.005,
+  },
+  {
+    "hour": 15,
+    "usage": 0.005,
+  },
+  {
+    "hour": 16,
+    "usage": 0.005,
+  },
+  {
+    "hour": 17,
+    "usage": 0.005,
+  },
+  {
+    "hour": 18,
+    "usage": 0.0783,
+  },
+  {
+    "hour": 19,
+    "usage": 0.0783,
+  },
+  {
+    "hour": 20,
+    "usage": 0.0783,
+  },
+  {
+    "hour": 21,
+    "usage": 0.0783,
+  },
+  {
+    "hour": 22,
+    "usage": 0.0783,
+  },
+  {
+    "hour": 23,
+    "usage": 0.0783,
+  },
+  {
+    "hour": 24,
+    "usage": 0.0783,
+  }
+]
+//Constant load profile (1/24), Hours 12 - 16 value is 0.01% higher to make the total exactly 100%.
+var constant = [
+  {
+    "hour": 1,
+    "usage": 0.04165,
+  },
+  {
+    "hour": 2,
+    "usage": 0.04165,
+  },
+  {
+    "hour": 3,
+    "usage": 0.04165,
+  },
+  {
+    "hour": 4,
+    "usage": 0.04165,
+  },
+  {
+    "hour": 5,
+    "usage": 0.04165,
+  },
+  {
+    "hour": 6,
+    "usage": 0.04165,
+  },
+  {
+    "hour": 7,
+    "usage": 0.04165,
+  },
+  {
+    "hour": 8,
+    "usage": 0.04165,
+  },
+  {
+    "hour": 9,
+    "usage": 0.04165,
+  },
+  {
+    "hour": 10,
+    "usage": 0.04165
+  },
+  {
+    "hour": 11,
+    "usage": 0.04165,
+  },
+  {
+    "hour": 12,
+    "usage": 0.04175,
+  },
+  {
+    "hour": 13,
+    "usage": 0.04175,
+  },
+  {
+    "hour": 14,
+    "usage": 0.04175,
+  },
+  {
+    "hour": 15,
+    "usage": 0.04175,
+  },
+  {
+    "hour": 16,
+    "usage": 0.04175,
+  },
+  {
+    "hour": 17,
+    "usage": 0.04165,
+  },
+  {
+    "hour": 18,
+    "usage": 0.04165,
+  },
+  {
+    "hour": 19,
+    "usage": 0.04165,
+  },
+  {
+    "hour": 20,
+    "usage": 0.04165,
+  },
+  {
+    "hour": 21,
+    "usage": 0.04165,
+  },
+  {
+    "hour": 22,
+    "usage": 0.04165,
+  },
+  {
+    "hour": 23,
+    "usage": 0.04165,
+  },
+  {
+    "hour": 24,
+    "usage": 0.04165,
+  }
+]
+
